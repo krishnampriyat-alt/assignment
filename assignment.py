c357069da0c146637cd7aff4358697be604ea31c
krishnampriyat@gmail.com <krishnampriyat@gmail.com>
2026-07-13 12:14:46 +0530

    First Commit

---
+++ assignment.py
@@ -0,0 +1,108 @@
+# print("krsihna")
+# print("python")
+# print("s")
+# name="krishna"
+# # 5rules for naming variable
+# #123name=krishna
+# #print(name)
+# #Name=name
+# name123="krishna"
+# _name="krishna"
+# """
+# kri
+# sh
+# na
+# priya
+# """
+#type function
+# ag=10
+# print(type(agee))
+# salary=10.556
+# print(type(salary))
+
+# age1='10' 
+# print(type(age1))
+
+#control+/
+#user input
+
+# name=input("enter your name=")
+# print(name)
+# print(type(name))
+
+# age=input("enter your age=")
+# print(age)a
+# print(type(age))
+# b=int(age)
+# print(type(b)print)
+# age=int(input("enter your age"))
+
+
+
+#string concatination
+# print("hello"+" world")
+# print("hello "+"world")
+# a=10
+# b=4
+# print(a*b)
+# print(a**b)
+# print(a%b)
+# print(a/b)
+# print(a//b)
+# #a=int(input("enter your number"))
+# x=5
+# print(x)
+# x*=2
+# print(x)
+# x=100
+# y=10
+# print(x==y)
+# print(x>y)
+# print(x!=y)
+# print(x>=y)
+
+#logical
+#and,or,not
+#and
+# a=10
+# b=5
+# c=3
+# print(a>b and b>c)
+# print(a>b or a>b)
+# print(a>b not a>b)
+# print (not(a>b or b>c))
+
+#membership
+#in,not in
+# a='string'
+# print('i' in a)
+# print('d' in a)
+# print('z' not in a)
+
+#identity
+#is not
+# a='string'
+# b='string'
+# print(a==b)
+# print(a is b)
+# print(id(a))
+# print(id(b))
+
+# a=[1,2,3,4]
+# b=[1,2,3]
+# print(a==b)
+# print(a is b)
+# print(id(a))
+# print(id(b))
+#conditional statements
+# age=int(input('enter your age'))
+# if condition:
+# age=int(input('enter your age='))
+# if condition:
+#     body
+# if age>=18:
+#     print('success')
+# else:
+#     print('age less than 18')
+
+
